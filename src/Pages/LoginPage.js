f6c9527a1281feac0d8ce934f5c34d9123a3414c
Ali ahmet Erdoğdu <aliahmeterdogdu@192.168.1.110>
2023-09-30 01:11:47 +0300

    mail sign in eklendi

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, View, Alert } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, Alert , KeyboardAvoidingView} from 'react-native'
 import React from 'react'
 import LottieView from 'lottie-react-native'
 import { Formik } from 'formik'
@@ -7,6 +7,8 @@ import LoginButton from '../Components/LoginButton'
 import RegisterPageInButton from '../Components/RegisterPageInButton'
 import Input from '../Components/Input'
 import { useDispatch } from 'react-redux'
+import auth from "@react-native-firebase/auth"
+
 const LoginPage = ({navigation}) => {
     const dispatch = useDispatch()
     const initialValues = {
@@ -18,22 +20,18 @@ const LoginPage = ({navigation}) => {
             Alert.alert("Hata","Boş bırakılamaz")
             return
         }
-        dispatch({type:"LOG_IN"})
 
+        auth().signInWithEmailAndPassword(values.email, values.password)
+        .then(() => {
+            console.log("User account signed ing ")
+            dispatch({type:"LOG_IN"})
 
+        }).catch(error => {
 
-        // auth().signInWithEmailAndPassword(values.email, values.password)
-        // .then(() => {
-        //     console.log("User account signed ing ")
-
-        // }).catch(error => {
-
-        //     if (error.code === 'auth/invalid-email') {
-        //       console.log('That email address is invalid!');
-        //     }
-        
-        // });
-
+            if (error.code === 'auth/invalid-email') {
+              console.log('That email address is invalid!');
+            }
+        });             
     }
 
     function goSingUp() {
@@ -42,6 +40,8 @@ const LoginPage = ({navigation}) => {
 
     return (
         <SafeAreaView style={{flex:1}}>
+                  <KeyboardAvoidingView style={{flex:1}}
+      behavior="padding">
             <LottieView source={require("../Assests/hRX7g0h08u.json")} useNativeLooping autoPlay loop style={{flex:0.9,}}></LottieView>
             <View style={{flex:1,}}>
             <Formik
@@ -58,6 +58,7 @@ const LoginPage = ({navigation}) => {
             </Formik>
             <RegisterPageInButton onPress={goSingUp} />
             </View>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
--- src/Pages/RegistrationPage.js
+++ src/Pages/RegistrationPage.js
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View,Alert, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View,Alert, SafeAreaView, KeyboardAvoidingView } from 'react-native'
 import React from 'react'
 import Input from '../Components/Input'
 import Icon from "react-native-vector-icons/Entypo"
 import LoginButton from '../Components/LoginButton'
 import {Formik} from "formik"
 import LottieView from 'lottie-react-native'
-//import auth from "@react-native-firebase/auth"
-
+import auth from "@react-native-firebase/auth"
+import { useDispatch } from 'react-redux'
 const Registration = ({navigation}) => {
+  const dispatch = useDispatch()
   const initialValues = {
     email: "",
     password: "",
@@ -25,24 +26,27 @@ const Registration = ({navigation}) => {
       return
     }
 
-    // auth().createUserWithEmailAndPassword(values.email, values.password).then(() => {
-    //   console.log('User account created & signed in!');
-    //   navigation.navigate("QrGeneratePage")
+    auth().createUserWithEmailAndPassword(values.email, values.password).then(() => {
+      console.log('User account created & signed in!');
+      dispatch({type:"LOG_IN"})
 
-    // })
-    // .catch(error => {
-    //   if (error.code === 'auth/email-already-in-use') {
-    //     console.log('That email address is already in use!');
-    //   }
+
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      }
   
-    //   if (error.code === 'auth/invalid-email') {
-    //     console.log('That email address is invalid!');
-    //   }
-    // }); 
+      if (error.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+    }); 
 }
 
   return (
     <SafeAreaView style={{flex:1}}>
+      <KeyboardAvoidingView style={{flex:1}}
+      behavior="padding">
       <LottieView source={require("../Assests/animation_ln3qynjc.json")} useNativeLooping autoPlay loop style={{flex:0.85,}}></LottieView>
       <Formik
         initialValues={initialValues}
@@ -57,6 +61,7 @@ const Registration = ({navigation}) => {
           </View>
         )}
       </Formik>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
